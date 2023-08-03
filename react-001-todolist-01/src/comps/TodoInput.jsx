e8d1216e2d9e2fbef450eaaec8138f07bb14ef36
callor <callor@callor.com>
2023-08-03 16:00:12 +0900

    todoList-v1

--- react-001-todolist-01/src/comps/TodoInput.jsx
+++ react-001-todolist-01/src/comps/TodoInput.jsx
@@ -1,16 +1,35 @@
 import { useState } from "react";
 
 // 여기는 Todo Content 를 입력하고 추가를 실행하는 Component
+/**
+ * @param {*} props
+ * 부모 Comps 로 부터 전달받은 모든 것을 담아 오는 바구니
+ * props 를 통하여 전달받은 모든 것은 "Read Only" 이다
+ * props 를 통하여 전달받은
+ *    state 도 여기에서 절대 변경 할수 없다
+ * props 통하여 전달받은 state 를 변경하려면
+ *    state 를 변경할 함수도 같이 전달받아야 한다
+ *
+ * @returns
+ */
 const TodoInput = (props) => {
   // 화면에 데이터를 Rendering 할때 사용할 State(변수) 선언
   // const [content, setContent] = useState("");
 
   // const [n1, n2] = ["대한민국","우리나라"]
-  const { content, setContent } = props;
+  const { content, setContent, todoListAdd } = props;
+  // const content1 = props.content;
+  // const setContent1 = props.setContent;
 
   const inputChangeHandler = (e) => {
     const value = e.target.value;
     setContent(value);
+  };
+
+  const btnClickHandler = (e) => {
+    // 추가 버튼을 클릭했을때 할일
+    todoListAdd(content);
+    setContent("");
   };
 
   return (
@@ -33,7 +52,9 @@ const TodoInput = (props) => {
       react 에서는 disabled={true} 라는 속성으로 사용한다
 
       */}
-      <button disabled={content.length < 2}>추가</button>
+      <button onClick={btnClickHandler} disabled={content.length < 2}>
+        저장
+      </button>
     </div>
   );
 };
--- react-001-todolist-01/src/comps/TodoMain.jsx
+++ react-001-todolist-01/src/comps/TodoMain.jsx
@@ -4,6 +4,11 @@ import "../css/Todo.css";
 import TodoList from "./TodoList";
 import { SampleData } from "../data/SampleTodo";
 
+// JS 에서 날짜와 관련된 여러가지 문제를 해결한 plug in
+// Date 라고 하는 날짜와 관련된 객체 있지만
+// 실무에서는 거의 moment 를 사용한다
+import moment from "moment";
+
 const TodoMain = () => {
   /**
    * State 끌어 올리기
@@ -22,13 +27,91 @@ const TodoMain = () => {
    * 부모 Comps 인 TodoMain 으로 끌어 올리기를 한다
    * 그리고, 자식 Comps 에 전달해주어야 한다
    */
+  const [todo, setTodo] = useState(null);
+
   const [content, setContent] = useState("");
-  const [todoList, setTodoList] = useState([...SampleData]);
+  const [todoList, setTodoList] = useState([]);
+
+  const todoListAdd = (todo) => {
+    const id = todoList[todoList.length - 1]?.id + 1 || 0;
+    const addTodo = {
+      id: id,
+      sdate: moment().format("YYYY[-]MM[-]DD"),
+      stime: moment().format("HH:mm:ss"),
+      content: todo,
+      complete: false,
+    };
+    setTodoList([...todoList, addTodo]);
+  };
+
+  const itemComplete = (id) => {
+    // 완료처리 예정
+
+    const compTodoList = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, complete: !todo.complete };
+      }
+      return todo;
+    });
+    setTodoList([...compTodoList]);
+  };
+
+  const itemDelete = (id) => {
+    // id 에 해당하는 데이터 삭제
+    if (window.confirm("정말 삭제 할거야?")) {
+      // list를 forEach 하면서 item의 id 와 일치하는 데이터가 있으면
+      // 해당 데이터를 제외하면서 새로운 리스트 만들기
+      // 전달받은 Id 와 일치하지 않은 item 만 모아서 새로운 배열 만들기
+      const deleteTodoList = todoList.filter((todo) => {
+        return todo.id !== id;
+      });
+      setTodoList([...deleteTodoList]);
+    }
+  };
+
+  // Content 를 클릭했을때 선택된 item 을 찾아주는 함수
+  const updateItemSelect = (id) => {
+    const selectTodoList = todoList.filter((todo) => {
+      return todo.id === id;
+    });
+    console.log(selectTodoList[0]);
+    setContent(selectTodoList[0].content);
+    // update 를 위한 임시 데이터 저장
+    setTodo({ ...selectTodoList[0] });
+  };
+
+  // 내용을 변경하고 저장을 클릭했을때 내용을 변경하는 함수
+  const updateItemOK = (text) => {
+    if (todo) {
+      const updateTodo = { ...todo, content: text };
+      const updateTodoList = todoList.map((item) => {
+        if (item.id === todo.id) {
+          return updateTodo;
+        }
+        return item;
+      });
+      setTodoList(updateTodoList);
+      setTodo(null);
+    } else {
+      todoListAdd(text);
+    }
+  };
 
   return (
     <div className="todo">
-      <TodoInput content={content} setContent={setContent} />
-      <TodoList todoList={todoList} />
+      <TodoInput
+        content={content}
+        setContent={setContent}
+        todoListAdd={updateItemOK}
+        // setTodoList={setTodoList}
+        // todoList={todoList}
+      />
+      <TodoList
+        itemDelete={itemDelete}
+        todoList={todoList}
+        itemComplete={itemComplete}
+        updateItemSelect={updateItemSelect}
+      />
     </div>
   );
 };
