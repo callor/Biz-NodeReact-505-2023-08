499a34c751e8e76cd562c70a40cecb4a53e4b437
callor <callor@callor.com>
2023-08-28 14:10:47 +0900

    exam

---
+++ exam/index.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+
+const multi =(num) =>{}
+
+const nums = [48,22,43]
+for(let i = 0 ; nums.length ; i++) {
+    const result = multi(nums[i])
+    if(result === ??) {
+        console.log(`${nums[i]} 테스트 케이이스는 3의 배수`)
+    }
+}
